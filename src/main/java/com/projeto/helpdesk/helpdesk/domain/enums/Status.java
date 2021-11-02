5e502a98fadbac7effed68200dcfec0ff16c9cfb
Breno Proti <moreirasgbr@gmail.com>
2021-11-02 17:26:20 -0300

    criação de enums

---
+++ src/main/java/com/projeto/helpdesk/helpdesk/domain/enums/Status.java
@@ -0,0 +1,36 @@
+package com.projeto.helpdesk.helpdesk.domain.enums;
+
+public enum Status {
+
+    ABERTO(0, "ABERTO"),
+    ANDAMENTO(1, "ANDAMENTO"),
+    ENCERRADO(2, "ENCERRADO");
+
+    private Integer codigo;
+    private String descricao;
+
+    Status(Integer codigo, String descricao) {
+        this.codigo = codigo;
+        this.descricao = descricao;
+    }
+
+    public Integer getCodigo() {
+        return codigo;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public static Status toEnum(Integer cod) {
+        if (cod == null) {
+            return null;
+        }
+        for (Status i : Status.values()) {
+            if (cod.equals(i.getCodigo())) {
+                return i;
+            }
+        }
+        throw new IllegalArgumentException("Status Inválido");
+    }
+}
