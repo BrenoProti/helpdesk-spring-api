15da100f92a935385b82c1db8c44db44e2e1f936
Breno Proti <moreirasgbr@gmail.com>
2021-11-02 17:11:47 -0300

    first commit

---
+++ src/test/java/com/projeto/helpdesk/helpdesk/HelpdeskApplicationTests.java
@@ -0,0 +1,13 @@
+package com.projeto.helpdesk.helpdesk;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelpdeskApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
